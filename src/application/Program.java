5f12d6b3812d21c23ceb23e6a03d5f07fb648a23
MarVinCan <marcus.v.e.candido@gmail.com>
2022-03-30 19:23:31 -0300

    Execucao do exercicio

--- src/application/Program.java
+++ src/application/Program.java
@@ -4,9 +4,11 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Locale;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 import entities.Product;
 
@@ -30,9 +32,16 @@ public class Program {
 
 				line = br.readLine();
 			}
+			double avg = list.stream().map(p -> p.getPrice()).reduce(0.0, (x, y) -> x + y) / list.size();
 			
+			System.out.println("Average price: "+ String.format("%.2f", avg));
 			
-			
+			Comparator<String> comp = (s1, s2) -> s1.toUpperCase().compareTo(s2.toUpperCase());
+
+			List<String> name = list.stream().filter(p -> p.getPrice() < avg).map(p -> p.getName())
+					.sorted(comp.reversed()).collect(Collectors.toList());
+
+			name.forEach(System.out::println);
 
 		} catch (IOException e) {
 			System.out.println("Error: " + e.getMessage());
